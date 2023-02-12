8d7e61f887f8a27dfa881d95f7d0aeb42bc2182d
adampgiles <adampgiles@outlook.com>
2023-02-12 22:34:03 +0000

    code: cleaned up code further

--- mini_tales/authentication/routes.py
+++ mini_tales/authentication/routes.py
@@ -1,13 +1,11 @@
-from flask import (flash, render_template,
-                   redirect, request, session,
-                   url_for, Blueprint)
+from flask import (flash, render_template, redirect, request, session, url_for, Blueprint)
 from werkzeug.security import generate_password_hash, check_password_hash
 from mini_tales import mongo
 
 # Create a Blueprint authentication object 
 authentication = Blueprint('authentication', __name__)
 
-@authentication.route("/register", methods=["GET", "POST"])
+@authentication.route("/register", methods = ["GET", "POST"])
 def register():
     """
     This function registers an account for a user in the users database collection. 
@@ -23,18 +21,18 @@ def register():
             flash("Username already exists")
             return redirect(url_for("authentication.register"))
         register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password")),
+            "username" : request.form.get("username").lower(),
+            "password" : generate_password_hash(request.form.get("password")),
             "liked_tales": []
         }
         mongo.db.users.insert_one(register)
         session["user"] = request.form.get("username").lower()
         flash("Account Successfully Created!")
         session["logged_in"] = True
-        return redirect(url_for("tales.mytales", username=session["user"])) 
+        return redirect(url_for("tales.mytales", username = session["user"])) 
     return render_template("authentication/register.html")
 
-@authentication.route("/login", methods=["GET", "POST"])
+@authentication.route("/login", methods = ["GET", "POST"])
 def login():
     """
     This function checks the user account exists and logs in the user if the username and password match.
@@ -43,13 +41,13 @@ def login():
     """
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username" : request.form.get("username").lower()})
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     session["logged_in"] = True
-                    return redirect(url_for("tales.mytales", username=session["user"]))
+                    return redirect(url_for("tales.mytales", username = session["user"]))
             else:
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("authentication.login"))
@@ -58,7 +56,7 @@ def login():
             return redirect(url_for("authentication.login"))
     return render_template("authentication/login.html")
 
-@authentication.route('/logout', methods=["GET","POST"])
+@authentication.route('/logout', methods = ["GET","POST"])
 def logout():
     """
     This function logs the user out of the site.
--- mini_tales/tales/routes.py
+++ mini_tales/tales/routes.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from flask import (flash, render_template,
-    redirect, request, session, url_for, Blueprint)
+from flask import (flash, render_template, redirect, request, session, url_for, Blueprint)
 from bson.objectid import ObjectId
 from mini_tales import mongo
 
@@ -8,7 +7,7 @@ from mini_tales import mongo
 tales = Blueprint('tales', __name__)
 
 @tales.route("/")
-@tales.route("/get_tales", methods=["GET","POST"])
+@tales.route("/get_tales", methods = ["GET","POST"])
 def get_tales():    
     """
     This function displays all tales in the tales collection on the base template.
@@ -27,19 +26,18 @@ def get_tales():
         tales = mongo.db.tales.find()        
     if request.method == "POST":         
         search = request.form.get("search-input") 
-        print(search) 
         if search == "":
             session["search"] = search
             tales = mongo.db.tales.find()
         else:            
-            tales = mongo.db.tales.find({"$text": { "$search": str(search) }})
+            tales = mongo.db.tales.find({"$text" : { "$search": str(search) }})
             session["search"] = search
-        return render_template("tales/tales.html", tales=tales)         
+        return render_template("tales/tales.html", tales = tales)         
     if session.get("logged_in") == True:
-        liked = mongo.db.users.find_one({"username": session["user"]})["liked_tales"]
+        liked = mongo.db.users.find_one({"username" : session["user"]})["liked_tales"]
         session["liked"] = liked
         session["fulltale"] = False 
-    return render_template("tales/tales.html", tales=tales)
+    return render_template("tales/tales.html", tales = tales)
 
 @tales.route("/clear_search", methods=["POST"])
 def clear_search():
@@ -48,18 +46,18 @@ def clear_search():
     """  
     session["search"] = ""
     tales = mongo.db.tales.find()
-    return render_template("tales/tales.html", tales=tales)
+    return render_template("tales/tales.html", tales = tales)
 
-@tales.route("/mytales/<username>", methods=["GET","POST"])
+@tales.route("/mytales/<username>", methods = ["GET","POST"])
 def mytales(username):
     """
     This function displays all of the currently logged in user's tales in the tales collection on the base template.
     """
-    username = mongo.db.users.find_one({"username": session["user"]})["username"]
-    tales = mongo.db.tales.find({"tale_author": session["user"]})    
-    return render_template("tales/mytales.html", username=username, tales=tales)
+    username = mongo.db.users.find_one({"username" : session["user"]})["username"]
+    tales = mongo.db.tales.find({"tale_author" : session["user"]})    
+    return render_template("tales/mytales.html", username = username, tales = tales)
 
-@tales.route("/newtale", methods=["GET", "POST"])
+@tales.route("/newtale", methods = ["GET", "POST"])
 def newtale():
     """
     This function displays a form that the user can use to submit a new tale to
@@ -69,21 +67,21 @@ def newtale():
     date = datetime.now()
     if request.method == "POST":
         usertale = {
-            "tale_title": request.form.get("title"),
-            "tale_blurb": request.form.get("blurb"),
-            "tale_topic": request.form.get("topic"),
-            "tale_content": request.form.get("tale-content"),
-            "tale_likes": 0,
-            "tale_views": 0,
-            "tale_publish_date": date.strftime("%d/%m/%Y"),
-            "tale_author": session["user"]
+            "tale_title" : request.form.get("title"),
+            "tale_blurb" : request.form.get("blurb"),
+            "tale_topic" : request.form.get("topic"),
+            "tale_content" : request.form.get("tale-content"),
+            "tale_likes" : 0,
+            "tale_views" : 0,
+            "tale_publish_date" : date.strftime("%d/%m/%Y"),
+            "tale_author" : session["user"]
         }
         mongo.db.tales.insert_one(usertale)
         flash("Tale Shared Successfully!")
-        return redirect(url_for("tales.mytales", username=session["user"]))
+        return redirect(url_for("tales.mytales", username = session["user"]))
     return render_template("tales/newtale.html") 
 
-@tales.route("/edittale/<_id>", methods=["GET","POST"])
+@tales.route("/edittale/<_id>", methods = ["GET","POST"])
 def edittale(_id): 
     """
     This function displays a pre-populated form that the user can use to edit one of their existing tales.
@@ -92,35 +90,35 @@ def edittale(_id):
     """
     date = datetime.now()
     _id = _id
-    tale = mongo.db.tales.find_one({"_id": ObjectId(_id)})
+    tale = mongo.db.tales.find_one({"_id" : ObjectId(_id)})
     if request.method == "POST":
         mongo.db.tales.update_one(
-            {"_id": ObjectId(_id)},
-            { "$set": {
-            "tale_title": request.form.get("title"),
-            "tale_blurb": request.form.get("blurb"),
-            "tale_topic": request.form.get("topic"),
-            "tale_content": request.form.get("tale-content"),
-            "tale_likes": 0,
-            "tale_views": 0,
-            "tale_publish_date": date.strftime("%d/%m/%Y")
+            {"_id" : ObjectId(_id)},
+            { "$set" : {
+            "tale_title" : request.form.get("title"),
+            "tale_blurb" : request.form.get("blurb"),
+            "tale_topic" : request.form.get("topic"),
+            "tale_content" : request.form.get("tale-content"),
+            "tale_likes" : 0,
+            "tale_views" : 0,
+            "tale_publish_date" : date.strftime("%d/%m/%Y")
             }})
-        mongo.db.users.update_many({"liked_tales": _id},{ "$pull": {"liked_tales": _id}})
+        mongo.db.users.update_many({"liked_tales" : _id},{ "$pull" : {"liked_tales" : _id}})
         flash("Tale Edited Successfully!")
-        return redirect(url_for("tales.mytales", username=session["user"]))
-    return render_template("tales/edittale.html", _id=_id, tale=tale)   
+        return redirect(url_for("tales.mytales", username = session["user"]))
+    return render_template("tales/edittale.html", _id = _id, tale = tale)   
 
-@tales.route("/deletetale/<_id>", methods=["GET","POST"])
+@tales.route("/deletetale/<_id>", methods = ["GET","POST"])
 def deletetale(_id):
     """
     This function takes the user to a page where they can confirm if 
     they are sure about deleting their existing tale.    
     """
     _id = _id
-    tale = mongo.db.tales.find_one({"_id": ObjectId(_id)}) 
-    return render_template("tales/deletetale.html", _id=_id, tale=tale) 
+    tale = mongo.db.tales.find_one({"_id" : ObjectId(_id)}) 
+    return render_template("tales/deletetale.html", _id = _id, tale = tale) 
 
-@tales.route("/confirmdeletetale/<_id>", methods=["GET","POST"])
+@tales.route("/confirmdeletetale/<_id>", methods = ["GET","POST"])
 def confirmdeletetale(_id): 
     """
     This function deletes the user's existing tale,
@@ -128,26 +126,26 @@ def confirmdeletetale(_id):
     The user is redirected to their My Tales page, once this change is actioned. 
     """
     _id = _id
-    mongo.db.tales.delete_one({"_id": ObjectId(_id)})
-    mongo.db.users.update_many({"liked_tales": _id},{ "$pull": {"liked_tales": _id}})
+    mongo.db.tales.delete_one({"_id" : ObjectId(_id)})
+    mongo.db.users.update_many({"liked_tales" : _id},{ "$pull" : {"liked_tales" : _id}})
     flash("Tale Deleted Successfully!")
-    return redirect(url_for("tales.mytales", username=session["user"]))   
+    return redirect(url_for("tales.mytales", username = session["user"]))   
 
-@tales.route("/tale/<_id>", methods=["GET","POST"])
+@tales.route("/tale/<_id>", methods = ["GET","POST"])
 def tale(_id): 
     """
     This function displays the full tale that was selected on the tales page.
     """   
     _id = _id
-    mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc": {"tale_views": 1}})
-    tale = mongo.db.tales.find_one({"_id": ObjectId(_id)}) 
+    mongo.db.tales.update_one({"_id" : ObjectId(_id)},{ "$inc ": {"tale_views": 1}})
+    tale = mongo.db.tales.find_one({"_id" : ObjectId(_id)}) 
     if session.get("logged_in") == True: 
-        liked = mongo.db.users.find_one({"username": session["user"]})["liked_tales"]
+        liked = mongo.db.users.find_one({"username" : session["user"]})["liked_tales"]
         session["liked"] = liked
         session["fulltale"] = True
-    return render_template("tales/tale.html", _id=_id, tale=tale)
+    return render_template("tales/tale.html", _id = _id, tale = tale)
 
-@tales.route("/like_tale/<_id>", methods=["GET","POST"])
+@tales.route("/like_tale/<_id>", methods = ["GET","POST"])
 def like_tale(_id): 
     """
     This function allows the logged in user to increase/decrease
@@ -156,17 +154,17 @@ def like_tale(_id):
     like a tale once.
     """ 
     _id = _id
-    liked = mongo.db.users.find_one({"username": session["user"]})["liked_tales"]
+    liked = mongo.db.users.find_one({"username" : session["user"]})["liked_tales"]
     if _id in liked:
-        mongo.db.users.update_one({"username": session["user"]},{ "$pull": {"liked_tales": _id}})
-        mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc": {"tale_likes": -1}})
+        mongo.db.users.update_one({"username" : session["user"]},{ "$pull" : {"liked_tales" : _id}})
+        mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc" : {"tale_likes" : -1}})
         if session.get("fulltale") == True:  
-            mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc": {"tale_views": -1}})      
+            mongo.db.tales.update_one({"_id" : ObjectId(_id)},{ "$inc" : {"tale_views" : -1}})      
     else:
-        mongo.db.users.update_one({"username": session["user"]},{ "$push": {"liked_tales": _id}})
-        mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc": {"tale_likes": 1}})
+        mongo.db.users.update_one({"username" : session["user"]},{ "$push" : {"liked_tales" : _id}})
+        mongo.db.tales.update_one({"_id" : ObjectId(_id)},{ "$inc" : {"tale_likes" : 1}})
         if session.get("fulltale") == True:  
-            mongo.db.tales.update_one({"_id": ObjectId(_id)},{ "$inc": {"tale_views": -1}}) 
-    liked = mongo.db.users.find_one({"username": session["user"]})["liked_tales"]
+            mongo.db.tales.update_one({"_id" : ObjectId(_id)},{ "$inc" : {"tale_views" : -1}}) 
+    liked = mongo.db.users.find_one({"username" : session["user"]})["liked_tales"]
     session["liked"] = liked
     return redirect(request.referrer)
